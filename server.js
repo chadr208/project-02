e1cee7037d1701fa4b8abad8b568009640d3b7c0
Chad Rajcooar <chadr208@gmail.com>
2023-03-26 22:41:02 -0400

    linked homepage to handlebars

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 // Provides utilities for working with file and directory paths
 const path = require('path');
 
+const { engine } = require('express-handlebars');
+
 // Define Variables 
 // Defining express as a top-level function to be re-used each time a request is made or the port is being listened to
 const app = express();
@@ -15,14 +17,17 @@ const routes = require('./routes')
 // This is useful when deploying the application to a hosting platform, as the hosting platform may specify the port that the application should listen on through an environment variable.
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
 // Initialize server
 app.use(routes)
-// GET Route for Homepage, '/' is the default filepath
-app.get('/', (req, res) => 
-  // The response for sending a get request to the homepage is to receive our index.html file
-  res.sendFile(path.join(__dirname, '/public/index.html')));
 
- 
 // When this file is ran, express listens for connections to our designated port address, and returns a node http.Server with this application as its callback
 sequelize.sync({force:false}).then(() => {
   app.listen(PORT, () =>
